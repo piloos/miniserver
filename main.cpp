631702bcf2e97166bd698f7fcad82bad2caba36c
Lode Cools <lode.cools@barco.com>
2017-09-25 09:45:56 +0200

    Add GET/PUT distinction

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <QCoreApplication>
 #include <QCommandLineParser>
 #include <QTimer>
+#include <QUrl>
 
 #include <iostream>
 #include <fstream>
 
 #include <qhttp/src/qhttpserver.hpp>
 #include <qhttp/src/qhttpserverresponse.hpp>
+#include <qhttp/src/qhttpserverrequest.hpp>
+#include <qhttp/src/qhttpfwd.hpp>
 
 using namespace std;
 
@@ -27,12 +30,25 @@ int main(int argc, char *argv[])
     server.listen( // listening on 0.0.0.0:8081
         QHostAddress::Any, 8081,
         [](QHttpRequest* req, QHttpResponse* res) {
-            Q_UNUSED(req);
-            // http status 200
-            res->setStatusCode(qhttp::ESTATUS_OK);
-            // the response body data
-            res->end("Hello World!\n");
-            // automatic memory management for req/res
+            req->collectData();
+
+            req->onEnd([req, res]() {
+                QUrl url = req->url();
+                cout << "Incoming URL: " << url.toString().toStdString() << endl;
+
+                if (req->method() == qhttp::EHTTP_PUT) {
+                    cout << "PUT method" << endl;
+                    QByteArray data = req->collectedData();
+                    int size = data.size();
+                    cout << "Data contains " << size << " elements" << endl;
+                }
+
+                // http status 200
+                res->setStatusCode(qhttp::ESTATUS_OK);
+                // the response body data
+                res->end("Hello World!\n");
+                // automatic memory management for req/res
+            });
     });
 
     if ( !server.isListening() ) {
